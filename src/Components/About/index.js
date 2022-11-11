15e828425033a02c14aa1cffd0e257664c74891f
rohitnair <rohit.nair@ucalgary.ca>
2022-11-10 20:04:38 -0700

    Formatted background colour and spacing

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -7,6 +7,28 @@ const About = () => {
             University of Calgary
             </h1>
             where I am studying for my BSc in Computer Science. I am very interested in Human-Computer interactions! I am also interested in:
+            <ul>
+                <li>
+                    Working out 💪🏽
+                </li>
+                <li>
+                    Developing personal projects 👨🏽‍💻
+                </li>
+                <li>
+                    Volunteering 🤝
+                </li>
+                <li>
+                    Entrepreneurship and innovation 📈
+                </li>
+                <li>
+                    Blockchain networks ⛓
+                </li>
+            </ul>
+            <div className='Button'>
+                <button>
+                  &#x3e;My resume&#60;
+                </button>
+            </div>
         </div>
     )
 }
